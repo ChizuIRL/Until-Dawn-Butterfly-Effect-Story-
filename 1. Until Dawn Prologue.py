eb291b70b6b19ea20432fce1bcb9c87639905988
ChizuIRL <97227757+ChizuIRL@users.noreply.github.com>
2022-01-06 19:12:55 +0800

    Add files via upload

---
+++ 1. Until Dawn Prologue.py
@@ -0,0 +1,329 @@
+# This is made by Kevin Sanchez
+import time
+
+# Prologue
+print("Winter, One Year Ago at 2:00 AM")
+
+time.sleep(1)
+print("""
+    The opening shot views the entrance and the back part of the lodge. Now it cuts to Beth through the window of the
+    kitchen while zooming backward.
+""")
+time.sleep(5)
+
+print("""
+--
+    A few seconds later, an unknown man is seen outside the lodge while holding a machete.
+    The scene then changes to a group of teenagers (Jessica, Emily, Mike, and Sam) talking about their forthcoming prank
+    on Beth's sister (Hannah).
+
+    Jessica holds a piece of paper and puts it on a nearby table.""")
+time.sleep(10)
+
+print("""
+--
+Emily: Oh, my God! I can't believe you actually did this.
+
+Jessica: Shhh... shhh... shhh...
+
+Sam: Don't you guys think this is a little bit cruel?
+
+Jess: Oh, come on, she deserves it.
+
+Sam: It's not her fault that she has a huge crush on Mike.
+
+Jess: Hannah's been making moves on him. I'm just looking out for my girl Em.
+
+    Jessica leaves the kitchen. Emily, Mike, and Sam follow her.
+
+Emily: Just because he's class Prez doesn't mean he belongs to everyone... Mike is my man.
+
+Mike: Hey, Em. I'm not anybody's man.
+
+Emily: Whatever you say, darling!
+
+    Emily, Jess, Mike, Matt, and Ashley head to the guest room, while Sam climbs up stairs to find and warn Hannah.
+
+Sam: Hannah!""")
+time.sleep(28)
+
+print("""
+--
+    In the other room, Ashley laughs as she crouches behind a partition, Matt hides inside the cabinets,and Jess and
+    Emily hide under the bed, while Mike waits for Hannah in the room.
+
+Emily and Jessica chuckle under the bed.
+
+Emily: Oop!... She's here. Shh!
+
+    The view switches to Hannah holding a candle while she heads to the guest room. She enters.
+
+Hannah: Mike? It's Hannah.
+
+Mike: Hey, Hannah.
+
+    The scene cuts and switches back to the view of Beth where she can be seen staring through the window silently at
+    the kitchen, where it can be seen that Josh and Chris are passed outafter drinking too much. It then again switches
+    to an unknown man outside the lodge, and he immediately leaves as Beth sees him through the window.
+
+Beth: Hey, did you see that? Dad said it'd just be us this weekend. Josh?""")
+time.sleep(38)
+beer_action = input("Interact with the bottle of beer? (yes/no)").lower().strip()
+
+if beer_action == "yes":
+    print("""
+--
+Beth: Jeez, Josh. Once again, brother, you've outdone us all.
+    As she is walking, Beth will go over to the table and find a note. She reads the letter written by Mike.""")
+else:
+    pass
+
+print("""
+
+        Hannah
+
+        You look so damn hot
+
+        In that shirt…
+
+        But I bet you’re even
+
+        Hotter out of it.
+
+        Come to the guest room
+
+        At 2:00am :)
+
+        Mike
+
+        XXX""")
+time.sleep(8)
+
+print("""
+--
+Beth: Oh, my God. What'd our naive sister get herself into now?
+
+    The scene cuts, and switches to Hannah and Mike.
+
+Hannah: I got your note.
+
+Mike: Glad you could make it.
+
+Mike: Maybe we should start with a little, you know, making out, and see where it goes from there.
+
+    Hannah is about to take off her blouse.
+
+Mike: Oh, hell yeah.
+
+    Jessica giggles as Hannah takes off her blouse.
+
+Jess: Oh, my God! She's taking her shirt off!
+
+Hannah notices.
+
+Hannah: What...? Oh, my God!
+
+    The others come out of their hiding places.
+
+Hannah: Matt? What are you doing here?!
+
+Mike: Uh, I'm sorry, Hannah, this all got outta hand, but....""")
+time.sleep(19)
+
+print("""
+--
+    Sam enters the guest room.
+
+Sam: Hannah! Hannah, hey, honey.... Don't... it's just a...
+
+Hannah: Mike!
+
+    Hannah exits the guest room, and runs out of the lodge.
+
+Sam: Stupid prank.
+
+Mike: Ugh... damn!
+
+Sam: You guys are jerks. You know that? 
+
+    Sam also exits the room and calls out for Hannah. Mike goes after her.
+
+Sam: Hannah!
+
+    The scene switches back to Beth, who is still reading the letter. She then looks at the window and notices Hannah
+    running outside.
+""")
+time.sleep(16)
+
+wake_up = input("Wake up Josh or Find others? (1) or (2) ")
+
+if wake_up == "1":
+    print("""
+    Beth runs over to Josh and shakes his shoulder. 
+Beth: Josh! Josh! Fuck.
+
+    Beth runs out the door to the main lodge.""")
+elif wake_up == "2":
+    print("""
+    Beth runs out the door to the main lodge.
+Beth: Guys, there's someone outside!""")
+else:
+    pass
+print("""
+--
+Sam: Hannah!
+
+    Beth catches up and shoves Matt out of her way.
+
+Beth: What's going on? Where's my sister going?
+
+Jess: Ugh, it's fine, she just can't take a joke...
+
+Emily: It was just a prank, Han!
+
+Beth: What did you do?
+
+Mike: We were just messing around, Beth. It wasn't serious...
+
+Beth: You jerks!
+
+Beth runs into the woods to look for Hannah.
+
+Beth: Hannah! Hannah!
+
+Mike: So... should we go after her?
+
+Sam: Y'know, I kinda think you're the last person she wants to see right now, Mike.
+
+    The scene switches to Beth while running into the woods to find Hannah.
+
+Beth: Oh, shit!""")
+time.sleep(22)
+
+beg_path = input("""
+--
+    Beth heard sounds of branches breaking. Follow footprints or Follow sounds? (1) / (2) """)
+
+if beg_path == "1":
+    print("""
+    Beth ran into a herd of elk, spooking them.
+
+Beth: Huh?! Jesus! Fuck, shit!""")
+elif beg_path == "2":
+    print("""
+Beth: Damn it. Hannah, where are you?""")
+else:
+    pass
+time.sleep(4)
+
+print("""
+--
+    Beth paused and used her phone as a flashlight.
+
+Beth: Hannah?""")
+time.sleep(4)
+
+print("Beth: Hello? Hello?!")
+time.sleep(2)
+
+print("""
+    Along the way, Beth found a totem. In the vision of the totem, is a death totem, showing Beth, and Hannah fell to
+    their deaths.""")
+time.sleep(7)
+
+print("""
+    A burst of flame bursts atop of a hill through a tree not to far from Beth""")
+time.sleep(7)
+
+print("""
+--
+Beth: What the hell was that?
+
+Beth: Hannah?
+
+Beth: Hannah?!
+
+Hannah: Hello?
+
+Beth: Hannah! Oh, my God, you must be freezing.
+
+    Beth comes toward Hannah, removing her coat.
+    
+Beth: Here, take my coat.
+
+Hannah stands up and takes her coat.
+
+Hannah: I'm such an idiot! I'm so dumb...""")
+time.sleep(14)
+
+print("""
+--
+    The view switches into a predator-like vision, who sights Hannah and Beth's heat signatures.They then later hear
+    a screech.
+
+Beth: Hannah...?
+
+Hannah: Beth...?
+
+    They both ran from the screech. They both reached the bridge, but Hannah fell.
+
+Beth: Hannah!
+
+    Beth helps Hannah stand up again, but as they run, Beth's phone falls through the floorboards. They continue
+    running, until they stop, because of the dead end they have run onto.
+
+Beth: Oh, Jesus Christ!""")
+time.sleep(19)
+
+print("""
+--
+    Both of them hold each other's hands and move backward from the thing that is coming for them at the cliff dead end.
+
+Beth: No!
+
+    View switches to the predator-like vision, still got the sight of the preys' movements.
+
+Beth: No, shit, no! Get back!
+
+    The view zooms to Hannah, and Beth's hand holding each other tightly and frightened.
+
+Beth: Fuck! 
+
+    Hannah and Beth walk back.
+
+Beth: No! Aah!
+
+    Both of them fall, but Beth held onto the branch attached under the cliff. They still managed to still survive for
+    awhile. Suddenly, a burst flame were shoot above them.
+
+Beth: Hold on!""")
+time.sleep(28)
+
+helping_hand = input("""
+--
+    An unknown man found both of them and lends them his hand. Drop Hannah or Let Go or do nothing? (1) / (2) / (3)""")
+
+if helping_hand == "1":
+    print("""
+Beth: I'm so sorry.
+    Beth drops Hannah. She tries to reach for the man's hand but the branch snaps and she falls off the cliff.""")
+    time.sleep(5)
+elif helping_hand == "2":
+    print("""
+    Beth looks at the man's hand and then at her sister. She lets go of the branch and they both fall off the cliff.""")
+    time.sleep(7)
+elif helping_hand == "3":
+    print("""
+    The branch snaps and they both fall off the cliff.""")
+    time.sleep(2)
+else:
+    pass
+
+
+print("""
+
+    As Beth falls, her back slams against the edge of a rock, killing her instantly.She tumbles down into the mines and
+    her corpse lands right next to her sister's body.""")
+time.sleep(8)
+print("(End of Prologue.)")
+print("""Hope you like my Interactive story! Any suggestions to improve it would be great!""")
